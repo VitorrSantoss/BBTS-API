1b8732ae3a22202b983246a32f7bbace503d4c11
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-11-04 15:44:25 -0300

    criacao do projeto spring boot

---
+++ src/main/java/br/com/bbts/api/bbts_api/BbtsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbts.api.bbts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbtsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbtsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bbts/api/bbts_api/BbtsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbts.api.bbts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbtsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
