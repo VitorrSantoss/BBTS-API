8e98501da40866096eb1c81f3209c5fe4f64840a
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-11-06 17:17:37 -0300

    estrutura para consulta no banco de dados

---
+++ src/main/java/br/com/bbts/api/bbts_api/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.bbts.api.bbts_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.bbts.api.bbts_api.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+  
+}
