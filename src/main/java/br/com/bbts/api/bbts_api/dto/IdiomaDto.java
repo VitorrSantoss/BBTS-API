aecf21e88866d5aa8b4f1dab331da88094e9c8c7
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-11-18 11:03:32 -0300

    cria DTOs para entidade Idioma

---
+++ src/main/java/br/com/bbts/api/bbts_api/dto/IdiomaDto.java
@@ -0,0 +1,15 @@
+package br.com.bbts.api.bbts_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IdiomaDto {
+  
+  private String nome;
+  private String nivel;
+  
+}
